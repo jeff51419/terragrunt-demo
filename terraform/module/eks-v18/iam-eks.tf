151ba7c08febc65b3b9e1e138e4a9ca62ef22aba
jeff51419 <jeff51419@gmail.com>
2022-03-18 17:15:51 +0800

    v18 terraform module

---
+++ terraform/module/eks-v18/iam-eks.tf
@@ -0,0 +1,378 @@
+#tfsec:ignore:aws-iam-no-policy-wildcards
+data "aws_iam_policy_document" "alb_ingress" {
+  #checkov:skip=CKV_AWS_111:sikp
+  #checkov:skip=CKV_AWS_109:sikp
+  statement {
+    sid = "1"
+    effect = "Allow"
+    actions = [
+      "iam:CreateServiceLinkedRole",
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeAddresses",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeInternetGateways",
+      "ec2:DescribeVpcs",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeInstances",
+      "ec2:DescribeNetworkInterfaces",
+      "ec2:DescribeTags",
+      "ec2:GetCoipPoolUsage",
+      "ec2:DescribeCoipPools",
+      "elasticloadbalancing:DescribeLoadBalancers",
+      "elasticloadbalancing:DescribeLoadBalancerAttributes",
+      "elasticloadbalancing:DescribeListeners",
+      "elasticloadbalancing:DescribeListenerCertificates",
+      "elasticloadbalancing:DescribeSSLPolicies",
+      "elasticloadbalancing:DescribeRules",
+      "elasticloadbalancing:DescribeTargetGroups",
+      "elasticloadbalancing:DescribeTargetGroupAttributes",
+      "elasticloadbalancing:DescribeTargetHealth",
+      "elasticloadbalancing:DescribeTags"
+    ]
+    resources = ["*"] 
+  }
+  statement {
+    sid = "2"
+    effect = "Allow"
+    actions = [
+      "cognito-idp:DescribeUserPoolClient",
+      "acm:ListCertificates",
+      "acm:DescribeCertificate",
+      "iam:ListServerCertificates",
+      "iam:GetServerCertificate",
+      "waf-regional:GetWebACL",
+      "waf-regional:GetWebACLForResource",
+      "waf-regional:AssociateWebACL",
+      "waf-regional:DisassociateWebACL",
+      "wafv2:GetWebACL",
+      "wafv2:GetWebACLForResource",
+      "wafv2:AssociateWebACL",
+      "wafv2:DisassociateWebACL",
+      "shield:GetSubscriptionState",
+      "shield:DescribeProtection",
+      "shield:CreateProtection",
+      "shield:DeleteProtection"
+    ]
+    resources = ["*"] 
+  }
+  statement {
+    sid = "3"
+    effect = "Allow"
+    actions = [
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupIngress"
+    ]
+    resources = ["*"] 
+  }
+  statement {
+    sid = "4"
+    effect = "Allow"
+    actions = [
+      "ec2:CreateTags"
+    ]
+    resources = ["*"] 
+  }
+  statement {
+    sid = "5"
+    effect = "Allow"
+    actions = [
+      "ec2:CreateSecurityGroup"
+    ]
+    resources = ["arn:aws:ec2:*:*:security-group/*"] 
+    condition {
+      test     = "StringEquals"
+      variable = "ec2:CreateAction"
+      values = [
+        "CreateSecurityGroup"
+      ]
+    }
+    condition {
+      test     = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values = [
+        "false"
+      ]
+    }
+  }  
+  statement {
+    sid = "6"
+    effect = "Allow"
+    actions = [
+      "ec2:CreateTags",
+      "ec2:DeleteTags"
+    ]
+    resources = ["arn:aws:ec2:*:*:security-group/*"] 
+    condition {
+      test     = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values = [
+        "true"
+      ]
+    }
+    condition {
+      test     = "Null"
+      variable = "aws:ResourceTag/elbv2.k8s.aws/cluster"
+      values = [
+        "false"
+      ]
+    }
+  }
+  statement {
+    sid = "7"
+    effect = "Allow"
+    actions = [
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupIngress",
+      "ec2:DeleteSecurityGroup"
+    ]
+    resources = ["*"] 
+    condition {
+      test     = "Null"
+      variable = "aws:ResourceTag/elbv2.k8s.aws/cluster"
+      values = [
+        "false"
+      ]
+    }
+  }
+  statement {
+    sid = "8"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:CreateLoadBalancer",
+      "elasticloadbalancing:CreateTargetGroup"
+    ]
+    resources = ["*"] 
+    condition {
+      test     = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values = [
+        "false"
+      ]
+    }
+  }
+  statement {
+    sid = "9"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:CreateListener",
+      "elasticloadbalancing:DeleteListener",
+      "elasticloadbalancing:CreateRule",
+      "elasticloadbalancing:DeleteRule"
+    ]
+    resources = ["*"] 
+  }
+  statement {
+    sid = "10"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:AddTags",
+      "elasticloadbalancing:RemoveTags"
+    ]
+    resources = [
+      "arn:aws:elasticloadbalancing:*:*:targetgroup/*/*",
+      "arn:aws:elasticloadbalancing:*:*:loadbalancer/net/*/*",
+      "arn:aws:elasticloadbalancing:*:*:loadbalancer/app/*/*"
+    ] 
+    condition {
+      test     = "Null"
+      variable = "aws:RequestTag/elbv2.k8s.aws/cluster"
+      values = [
+        "true"
+      ]
+    }
+    condition {
+      test     = "Null"
+      variable = "aws:ResourceTag/elbv2.k8s.aws/cluster"
+      values = [
+        "false"
+      ]
+    }
+  }
+  statement {
+    sid = "11"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:AddTags",
+      "elasticloadbalancing:RemoveTags"
+    ]
+    resources = [
+      "arn:aws:elasticloadbalancing:*:*:listener/net/*/*/*",
+      "arn:aws:elasticloadbalancing:*:*:listener/app/*/*/*",
+      "arn:aws:elasticloadbalancing:*:*:listener-rule/net/*/*/*",
+      "arn:aws:elasticloadbalancing:*:*:listener-rule/app/*/*/*"
+    ] 
+  }
+  statement {
+    sid = "12"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:ModifyLoadBalancerAttributes",
+      "elasticloadbalancing:SetIpAddressType",
+      "elasticloadbalancing:SetSecurityGroups",
+      "elasticloadbalancing:SetSubnets",
+      "elasticloadbalancing:DeleteLoadBalancer",
+      "elasticloadbalancing:ModifyTargetGroup",
+      "elasticloadbalancing:ModifyTargetGroupAttributes",
+      "elasticloadbalancing:DeleteTargetGroup"
+    ]
+    resources = ["*"] 
+    condition {
+      test     = "Null"
+      variable = "aws:ResourceTag/elbv2.k8s.aws/cluster"
+      values = [
+        "false"
+      ]
+    }
+  }
+  statement {
+    sid = "13"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:RegisterTargets",
+      "elasticloadbalancing:DeregisterTargets"
+    ]
+    resources = ["arn:aws:elasticloadbalancing:*:*:targetgroup/*/*"] 
+  }
+  statement {
+    sid = "14"
+    effect = "Allow"
+    actions = [
+      "elasticloadbalancing:SetWebAcl",
+      "elasticloadbalancing:ModifyListener",
+      "elasticloadbalancing:AddListenerCertificates",
+      "elasticloadbalancing:RemoveListenerCertificates",
+      "elasticloadbalancing:ModifyRule"
+    ]
+    resources = ["*"] 
+  }
+}
+resource "aws_iam_policy" "alb_ingress" {
+  name   = "${local.name}-ALBIngress"
+  path   = "/"
+  policy = data.aws_iam_policy_document.alb_ingress.json
+}
+
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+data "aws_iam_policy_document" "ecr_access" {
+  #checkov:skip=CKV_AWS_111:sikp
+  statement {
+    sid = "AllowPushPull"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchGetImage",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:CompleteLayerUpload",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:InitiateLayerUpload",
+      "ecr:PutImage",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:UploadLayerPart"
+    ]
+    resources = ["*"] 
+  }
+}
+resource "aws_iam_policy" "ecr_access" {
+  name   = "${local.name}-ecr_access"
+  path   = "/"
+  policy = data.aws_iam_policy_document.ecr_access.json
+}
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+data "aws_iam_policy_document" "autoscaler_access" {
+  #checkov:skip=CKV_AWS_111:sikp
+  statement {
+    sid = "1"
+    effect = "Allow"
+    actions = [
+      "autoscaling:DescribeAutoScalingGroups",
+      "autoscaling:DescribeAutoScalingInstances",
+      "autoscaling:DescribeLaunchConfigurations",
+      "autoscaling:DescribeTags",
+      "autoscaling:SetDesiredCapacity",
+      "autoscaling:TerminateInstanceInAutoScalingGroup",
+      "ec2:DescribeLaunchTemplateVersions"
+    ]
+    resources = ["*"] 
+  }
+}
+resource "aws_iam_policy" "autoscaler_access" {
+  name   = "${local.name}-autoscaler_access"
+  path   = "/"
+  policy = data.aws_iam_policy_document.autoscaler_access.json
+}
+
+
+#tfsec:ignore:aws-iam-no-policy-wildcards
+data "aws_iam_policy_document" "elasticfilesystem_access" {
+  #checkov:skip=CKV_AWS_111:sikp
+  statement {
+    sid = "1"
+    effect = "Allow"
+    actions = [
+      "elasticfilesystem:DescribeAccessPoints",
+      "elasticfilesystem:DescribeFileSystems"
+    ]
+    resources = ["*"] 
+  }
+  statement {
+    sid = "2"
+    effect = "Allow"
+    actions = [
+      "elasticfilesystem:CreateAccessPoint"
+    ]
+    resources = ["*"] 
+    condition {
+      test     = "StringLike"
+      variable = "aws:RequestTag/efs.csi.aws.com/cluster"
+
+      values = [
+        "true"
+      ]
+    }
+  }
+  statement {
+    sid = "3"
+    effect = "Allow"
+    actions = [
+      "elasticfilesystem:DeleteAccessPoint"
+    ]
+    resources = ["*"] 
+    condition {
+      test     = "StringEquals"
+      variable = "aws:ResourceTag/efs.csi.aws.com/cluster"
+
+      values = [
+        "true"
+      ]
+    }
+  }
+}
+resource "aws_iam_policy" "elasticfilesystem_access" {
+  name   = "${local.name}-elasticfilesystem_access"
+  path   = "/"
+  policy = data.aws_iam_policy_document.elasticfilesystem_access.json
+}
+
+resource "aws_iam_role_policy_attachment" "alb_ingress" {
+  role       = module.eks.cluster_iam_role_name
+  policy_arn = aws_iam_policy.alb_ingress.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_access" {
+  role       = module.eks.cluster_iam_role_name
+  policy_arn = aws_iam_policy.ecr_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "autoscaler_access" {
+  role       = module.eks.cluster_iam_role_name
+  policy_arn = aws_iam_policy.autoscaler_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "elasticfilesystem_access" {
+  role       = module.eks.cluster_iam_role_name
+  policy_arn = aws_iam_policy.elasticfilesystem_access.arn
+}
+
